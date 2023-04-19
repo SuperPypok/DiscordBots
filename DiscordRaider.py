44691cbaaae471c250b3d1c78d0b66c6e6dfde17
SuperPypok <112332753+SuperPypok@users.noreply.github.com>
2023-04-19 23:30:28 +0700

    Add files via upload

---
+++ DiscordRaider.py
@@ -0,0 +1,18 @@
+import discord
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    channel = bot.get_channel(1085938113420988506) # Your channel id
+    # for guild in bot.guilds:        # These two lines are responsible for displaying the
+    #     print(guild.name)           # names of all those servers where the bot is present.
+    guild = bot.guilds[0] # Get the first guild the bot is connected to
+    while True:
+        await channel.send("text") # Your text
+        await guild.create_text_channel("channel_name") # The channel name must be less than 100 characters long.
+
+bot.run("YOUR TOKEN")
